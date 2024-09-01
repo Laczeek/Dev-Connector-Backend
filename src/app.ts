3d12b9ea6f380ef6f62aa2169624d6a479c5e4fb
Patryk Popiela <patryk150500@gmail.com>
2024-09-01 15:21:25 +0200

    Add logic for mongodb server connection.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 8080;
+const MONGO_URL = process.env.MONGO_URL!;
 
 const app = express();
+mongoose.connect(MONGO_URL);
 
 app.use((req, res, next) => {
 	res.send('Hello!');
 });
 
-app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT} port.`);
+mongoose.connection.on('open', () => {
+	console.log('Connection with mongo server established.');
+	app.listen(PORT, () => {
+		console.log(`Server listening on ${PORT} port.`);
+	});
 });
-
 process.on('uncaughtException', err => {
 	console.error('Error occured 💥.');
 	console.error(err);
