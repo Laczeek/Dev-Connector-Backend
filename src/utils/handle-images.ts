2ef1458a6ce5612ac33aec55d7cd4c2b756a05c7
Patryk Popiela <patryk150500@gmail.com>
2024-09-10 23:32:13 +0200

    Add file filter for multer

--- src/utils/handle-images.ts
+++ src/utils/handle-images.ts
@@ -1,11 +1,21 @@
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import path from 'path';
 import fs from 'fs/promises';
 import sharp from 'sharp';
 import { v4 as uuid } from 'uuid';
+import { Request } from 'express';
+import AppError from './AppError';
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+	if (!file || file.mimetype.split('/')[0] !== 'image') {
+		return cb(new AppError('Wrong type of file. Only image allowed.', 400));
+	}
+
+	cb(null, true);
+};
 
 const storage = multer.memoryStorage();
-export const upload = multer({ storage });
+export const upload = multer({ storage, fileFilter, limits: { fileSize: 1024 * 1024 * 4 } });
 
 export const storeImage = async (buffer: Buffer, fileName: string, folder: string, width: number, height: number) => {
 	const uniqueFileName = `${fileName}-${uuid()}.jpeg`;
